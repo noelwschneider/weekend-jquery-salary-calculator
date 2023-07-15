3b7f43741610bf9eb473f941be41ffc0d899cdc8
noelwschneider <noelwschneider@gmail.com>
2023-07-15 10:16:40 -0500

    created HTML structure, listener for submit button, variables to hold input data

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(onReady);
+
+function onReady() {
+    
+    // Listener for the submit button
+    $('#submit-button').on('click', handleSubmit)
+}
+
+function handleSubmit() {
+    
+    // Variables to store input data
+    let firstName = $('#first-name-input').val();
+    let lastName = $('#last-name-input').val();
+    let id = $('#id-input').val();
+    let title = $('#title-input').val();
+    let annualSalary = $('#annual-salary-input').val();
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(id);
+    console.log(title);
+    console.log(annualSalary);
+}
