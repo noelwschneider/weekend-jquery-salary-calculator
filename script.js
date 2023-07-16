8d75f37db711746d870cc1d0a3f1e78fba6f5437
noelwschneider <noelwschneider@gmail.com>
2023-07-16 13:31:57 -0500

    outlined styling

--- script.js
+++ script.js
@@ -10,12 +10,17 @@ function onReady() {
 // Variable for collecting employee salaries
 let monthlyCosts = 0;
 
+// Variable for determining table row color
+let tableRowCounter = 0;
+
 // Function to delete table rows
 function deleteRow() {
 
     // Subtracting monthly costs for the removed employee
     let removedSalary = $(this).parent().siblings('.annual-salary-entry').text();
     monthlyCosts -= removedSalary / 12;
+
+    // Conditional to add red background if monthly costs are more than $20,000
     if(monthlyCosts > 20000) {
         $('#monthly-costs-value').html(`<span id="monthly-costs-value" class="over-budget">${monthlyCosts}</span>`)
     } else {
@@ -24,13 +29,14 @@ function deleteRow() {
 
     // Removing the row
     $(this).parent().parent().remove();
-}
+} // end of deleteRow
 
 // function to:
     // Gather user data
     // Send user data to the table
     // Add and display monthly costs (based on salary input)
 function handleSubmit() {
+    
     // Variables to store input data
     let firstName = $('#first-name-input').val();
     let lastName = $('#last-name-input').val();
@@ -54,9 +60,18 @@ function handleSubmit() {
     $('#title-input').val('');
     $('#annual-salary-input').val('');
 
+    // Determining row color
+    tableRowCounter++;
+    let rowColorClass;
+    if(tableRowCounter % 2 === 0) {
+        rowColorClass = 'silver-row';
+    } else {
+        rowColorClass = 'white-row';
+    }
+
     // Adding a row to the table
     $('#employee-table-body').append(`
-    <tr>
+    <tr class='${rowColorClass}'>
         <td>${firstName}</td>
         <td>${lastName}</td>
         <td>${id}</td>
@@ -65,4 +80,4 @@ function handleSubmit() {
         <td><button class="delete-button">Delete</button></td>
     </tr>
     `); // end of append
-}
+} // end of handleSubmit()
